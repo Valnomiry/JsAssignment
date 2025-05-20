46ecd094f399c727b51851e7380d26d1a71016d2
Valnomiry <valnomiry@gmail.com>
2025-05-20 16:42:59 +0300

    enhancement

--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
-//
-qoutList = [
-    '“Be yourself; everyone else is already taken.”',
-    '“Two things are infinite: the universe and human stupidity; ”',
-    '“A room without books is like a body without a soul.”',
-    '“Be who you are and say what you feel, because those who mind don’t matter, and those who matter don’t mind.”',
-    '“You only live once, but if you do it right, once is enough.”'
-    , '“A friend is someone who knows all about you and still loves you.”'
-]
+//declare
 
-qoutAuthorList = [
-    '― Oscar Wilde',
-    '― Albert Einstein',
-    '― Marcus Tullius Cicero',
-    '― Bernard M. Baruch',
-    '― Mae West',
-    '― Elbert Hubbard'
+var qout = {
+    qouttext: "",
+    qoutAuthor: ""
+
+}
+qoutList = [
+    {
+        qouttext: "“Be yourself; everyone else is already taken.”",
+        qoutAuthor: "― Oscar Wilde"
+    },
+    {
+        qouttext: "“Two things are infinite: the universe and human stupidity; ”",
+        qoutAuthor: "― Albert Einstein"
+    },
+    {
+        qouttext: "“A room without books is like a body without a soul.”",
+        qoutAuthor: "― Marcus Tullius Cicero"
+    },
+    {
+        qouttext: "“Be who you are and say what you feel, because those who mind don’t matter, and those who matter don’t mind.”",
+        qoutAuthor: "― Bernard M. Baruch"
+    },
+    {
+        qouttext: "“You only live once, but if you do it right, once is enough.”",
+        qoutAuthor: "― Mae West"
+    },
+    {
+        qouttext: "“A friend is someone who knows all about you and still loves you.”",
+        qoutAuthor: "― Elbert Hubbard"
+    },
 ]
-// getNewQuto = () => {
-//     var lastIndex;
-//     var qout = document.getElementById('quote')
-//     var author = document.getElementById('quoteAuthor')
-//     var qoutIndex = Math.floor(Math.random() * qoutList.length)
-//     qout.innerHTML = qoutList[qoutIndex]
-//     author.innerHTML = qoutAuthorList[qoutIndex]
-// }
 
 
 
@@ -33,14 +40,14 @@ var lastIndex;
 var qout = document.getElementById('quote')
 var author = document.getElementById('quoteAuthor')
 
-getNewQuto = () => {
+function getNewQuto() {
 
 
     qoutIndex = Math.floor(Math.random() * qoutList.length);
     while (qoutIndex == lastIndex) {
         qoutIndex = Math.floor(Math.random() * qoutList.length);
     }
-    qout.innerHTML = qoutList[qoutIndex];
-    author.innerHTML = qoutAuthorList[qoutIndex];
+    qout.innerHTML = qoutList[qoutIndex].qouttext;
+    author.innerHTML = qoutList[qoutIndex].qoutAuthor;
     lastIndex = qoutIndex;
 }
